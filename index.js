7b60d1d75f852567e6e2f972258d58dc5ea3fba2
nileshp2906 <nileshpawar2906@gmail.com>
2023-01-31 20:26:46 +0530

    pan se;ected js completed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// FORM VALIDAYION VARIABLES
 const names = document.getElementById("name");
 const email = document.getElementById("email");
 const mobileNo = document.getElementById("mobile");
@@ -7,24 +8,23 @@ const nameError = document.getElementById("name-error");
 const emailError = document.getElementById("email-error");
 const noError = document.getElementById("no-error");
 const nextBtn = document.querySelector(".next-step-btn");
+
 const nameRegex = /[A-Za-z]+[^0-9_]{3,29}$/;
 const emailRegex = /^[a-z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
 const mobileRegex = /^(\+\d{1,3}[- ]?)?\d{10}$/;
 
-
-
-
+// FORM VALIDATION CODE FOR FIRST PAGE
 nextBtn.addEventListener("click",(e)=>{
   inputs.forEach((i)=>{
     if(i.value == ""){
         if(i.dataset.no == "1"){
-            nameError.innerHTML ="hello"
+            nameError.innerHTML ="This field is requied *";
         }
         if(i.dataset.no == "2"){
-            emailError.innerHTML ="hello"
+            emailError.innerHTML ="This field is requied *";
         }
         if(i.dataset.no == "3"){
-            noError.innerHTML ="hello"
+            noError.innerHTML ="This field is requied *";
         }
     }
     
@@ -39,7 +39,7 @@ nextBtn.addEventListener("click",(e)=>{
             noError.innerHTML ="";
         }
     }
-  })
+});
   
     if(!nameRegex.test(names.value) && names.value !== ""){
         nameError.innerHTML= "Invalid name ";
@@ -59,3 +59,53 @@ nextBtn.addEventListener("click",(e)=>{
 
 });
 
+// VARIABLE FOR SECOND PAGE OF PLANS
+const planList = document.querySelectorAll(".plan-list");
+const checkBox = document.querySelector(".checkbox");
+const pricePlan = document.querySelectorAll(".plan-price")
+
+// PLAN SELECTOR CODE TOGGLE BUTTON SELECTOR
+
+const tempPlan = []
+planList.forEach((e,index)=>{
+    e.addEventListener("click",(on)=>{
+        for(let i of planList) {
+            i.classList.remove("active");
+        }
+        on.currentTarget.classList.add("active");
+    });
+});
+
+// VALUE SELECT PLAN
+checkBox.addEventListener("change",()=>{
+    if(checkBox.checked){
+        pricePlan.forEach((e)=>{
+            if(e.dataset.plan == "acrade") {
+                e.innerHTML ="$90/yr";
+            }
+    
+            if(e.dataset.plan == "advanced") {
+                e.innerHTML ="$120/yr";
+            }
+    
+            if(e.dataset.plan == "pro") {
+                e.innerHTML ="$150/yr";
+            }
+        });
+    }
+    else {
+        pricePlan.forEach((e)=>{
+            if(e.dataset.plan == "acrade") {
+                e.innerHTML ="$9/mo";
+            }
+    
+            if(e.dataset.plan == "advanced") {
+                e.innerHTML ="$12/mo";
+            }
+    
+            if(e.dataset.plan == "pro") {
+                e.innerHTML ="$15/mo";
+            }
+        })
+    }
+});
